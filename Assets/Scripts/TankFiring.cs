b7dba8fed89494f091846d1ac96553ed988bbe9a
Poomnapat <poomnapat.tumr@bumail.net>
2025-03-27 19:00:29 +0700

    Added Sounds and create new tank + movement

--- Assets/Scripts/TankFiring.cs
+++ Assets/Scripts/TankFiring.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Cloth : MonoBehaviour
+public class TankFiring : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
