3711ae637251778b10171185006fd742a39dad92
Poomnapat <poomnapat.tumr@bumail.net>
2025-03-30 21:14:37 +0700

    เพิ่มฉาก

--- Assets/Scripts/TankShooting.cs
+++ Assets/Scripts/TankShooting.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class TankShooting : MonoBehaviour
 {
@@ -6,10 +7,18 @@ public class TankShooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float bulletForce = 20f;
+    public AudioClip shootSound;
+    public AudioSource m_ShootingAudio;
 
     [Header("Fire Rate")]
     public float fireCooldown = 0.5f;
     private float fireTimer = 0f;
+
+    void Start()
+    {
+        m_ShootingAudio = GetComponent<AudioSource>(); 
+    }
+
 
     void Update()
     {
@@ -30,6 +39,11 @@ public class TankShooting : MonoBehaviour
         if (rb != null)
         {
             rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+        }
+
+        if (shootSound != null && m_ShootingAudio != null)
+        {
+            m_ShootingAudio.PlayOneShot(shootSound);
         }
     }
 }
